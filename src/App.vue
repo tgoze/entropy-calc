24d57855340633d26b91eb53e0bd5912b0242581
TJ Goze <tjgoze@gmail.com>
2019-11-14 12:37:25 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>  
+  <div id="app">    
+    <EntropyCalc />
+  </div>
+</template>
+
+<script>
+import EntropyCalc from './components/EntropyCalc.vue'
+
+export default {
+  name: 'app',
+  components: {  
+    EntropyCalc
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/EntropyCalc.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <label for="alphabetInput">Input a source alphabet with symbols separated by commas </label>
+        <input id="alphabetInput" type="text" v-on:keyup="getAlphabet($event.target.value)"/>
+        <label for="messageInput">Input message using your </label>
+        <input id="messageInput" type="text" v-on:keyup="calculateEntropy($event.target.value)" v-on:keypress="checkInput($event.key)"/>
+    </div>  
+</template>
+
+<script>
+export default {
+  name: 'EntropyCalc',
+  props: {
+    message: Array,
+    entropy: Number
+  },
+  methods: {
+      getAlphabet: function (input) {
+          var inputArray = input.split(",");
+          var alphabet = Array.from(new Set(inputArray));          
+      },
+      checkInput: function (key) {
+          
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
